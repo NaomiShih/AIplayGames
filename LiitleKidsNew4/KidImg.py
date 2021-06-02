07b935878bbd9a15ce386f50dee53b4d03361ad6
john0927626983 <84298954+john0927626983@users.noreply.github.com>
2021-06-02 18:41:23 +0800

    Add files via upload

---
+++ LiitleKidsNew4/KidImg.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 11:15:17 2021
+
+@author: USER
+"""
+import pygame
+from pygame.locals import *
+
+life = pygame.image.load('./assets/LIFE123.PNG')
+player = pygame.image.load('./assets/playerNomal.png')
+
+Normal = pygame.image.load('./assets/playerNomal.png')
+Board = pygame.image.load('./assets/normal.png')
+Nails = pygame.image.load('./assets/nails.png')
+
+
+
+wall =  pygame.image.load('./assets/wall.png')
+ceil = pygame.image.load('./assets/ceiling.png')
+TrampolineUP = pygame.image.load('./assets/trampolineUP.png')
+TrampolineDown = pygame.image.load('./assets/trampolineDown.png')
+conveyor_left = pygame.image.load('./assets/conveyor_left1.png')
+conveyor_right = pygame.image.load('./assets/conveyor_right1.png')

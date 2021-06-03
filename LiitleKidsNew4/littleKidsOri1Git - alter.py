391bc2586b748e60de4a6309411d6e28a147803c
john0927626983 <84298954+john0927626983@users.noreply.github.com>
2021-06-03 14:45:27 +0800

    Add files via upload

--- LiitleKidsNew4/littleKidsOri1Git - alter.py
+++ LiitleKidsNew4/littleKidsOri1Git - alter.py
@@ -10,7 +10,7 @@ import time
 import random
 from playsound import playsound
 import KidImg
-
+from itertools import cycle
 
 
 
@@ -133,56 +133,56 @@ class Platform:
         else:
             Board.Y -= 0.8
         
-        if Board.Y < 100:       
+        if Board.Y < 100 or Board.Y > 1240:       
             Board.X = random.randint(114,366)
             Board.Y = random.randint(740,1240)
             
             BoardYlist = [Board1.Y,Board2.Y,Board3.Y,Nails1.Y,Nails2.Y,Nails3.Y,Nails4.Y,Trampoline.Y,conveyor_left.Y,conveyor_right.Y]
             for i in BoardYlist[1:]:
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:1]+BoardYlist[2:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:2]+BoardYlist[3:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:3]+BoardYlist[4:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:4]+BoardYlist[5:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:5]+BoardYlist[6:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:6]+BoardYlist[7:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:7]+BoardYlist[8:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
                     
             for i in (BoardYlist[:8]+BoardYlist[9:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
             
             for i in (BoardYlist[:9]+BoardYlist[10:]):
                 if (Board.Y-i)**2 < 1400:
-                    Board.Y += 30
+                    Board.Y += 50
             
 
     def Platform_right(self, player):
-        player.X += player.vel-0.2
+        player.X += player.vel/2
     def Platform_left(self, player):
-        player.X -= player.vel-0.2
+        player.X -= player.vel/2
     def Platform_Rect(Platform, Player, PlatWidth, PlatHigth, PlayerWidth, PlayerHight, CountF):
         PlatformRect  = pygame.Rect(Platform.X, Platform.Y , PlatWidth, PlatHigth)
         PlayerRect    = pygame.Rect(Player.X, Player.Y, PlayerWidth, PlayerHight)
@@ -229,6 +229,7 @@ class Platform:
         pass
         
 
+     
     
   
 for i in range(0, 100, 1):           
@@ -240,16 +241,16 @@ RandX = random.randint(0, 99)
 
 
 Board1  = Platform(240, 600, -1, KidImg.Board)
-Nails1   = Platform(Xlist[4], 800, 5,KidImg.Nails )
-Nails2   = Platform(Xlist[2], random.randint(1100, 1200), 5,KidImg.Nails )
-Nails3   = Platform(Xlist[7], random.randint(1100, 1200), 5,KidImg.Nails )
-Nails4   = Platform(Xlist[5], random.randint(1100, 1200), 5,KidImg.Nails )
-Board2  = Platform(Xlist[3], 800, -1, KidImg.Board )
-Board3  = Platform(Xlist[3], 1300, -1, KidImg.Board)
-Trampoline  = Platform(Xlist[6], 900, -1, KidImg.TrampolineUP)
+Nails1   = Platform(Xlist[4], 1241, 5,KidImg.Nails )
+Nails2   = Platform(Xlist[2], 1241, 5,KidImg.Nails )
+Nails3   = Platform(Xlist[7], 1241, 5,KidImg.Nails )
+Nails4   = Platform(Xlist[5], 1241, 5,KidImg.Nails )
+Board2  = Platform(Xlist[3], 1241, -1, KidImg.Board )
+Board3  = Platform(Xlist[3], 1241, -1, KidImg.Board)
+Trampoline  = Platform(Xlist[6], 1241, -1, KidImg.TrampolineUP)
 ceil = Platform(20, 80, 5,  KidImg.ceil )
-conveyor_left = Platform(Xlist[5], 700, -1,  KidImg.conveyor_left)
-conveyor_right = Platform(Xlist[6], 600*2, -1,  KidImg.conveyor_right )
+conveyor_left = Platform(Xlist[5], 1241, -1,  KidImg.conveyor_left)
+conveyor_right = Platform(Xlist[6], 1241, -1,  KidImg.conveyor_right )
 
 
 timeflag = 0
@@ -273,9 +274,12 @@ while run:
     #print(boardtime)#測試秒數到哪裡 程式有沒有預期跑
     if times < 3 and player1.Y <= 640:
         CountF += 1
+    
+    
     elif player1.Y >= 640:
          playsound('./assets/sounds/Fall 2.mp3', block=True)
          run = False
+    
          
     head_font = pygame.font.SysFont(None, 60)#設定大小60的標題框框
     text_surface = head_font.render('B%04dF'%CountF, True, (121, 255, 121))#設定標題的字跟顏色
@@ -305,15 +309,13 @@ while run:
    
  
     WallRect  = pygame.Rect(0, 80, 18, 560)
-    WallRect2  = pygame.Rect(450, 80, 15, 560)
+    WallRect2  = pygame.Rect(450, 80, 18, 560)
 
     
     if (pygame.Rect.colliderect(playerRect, WallRect) == 1 ):
-        
-         player1.X  += player1.vel
-    if (pygame.Rect.colliderect(playerRect, WallRect2) == 1 ):
-         
-         player1.X  -= player1.vel
+         player1.X  = 18
+    elif (pygame.Rect.colliderect(playerRect, WallRect2) == 1 ):
+         player1.X  = 419
     
       
     if timeflag == 0:
@@ -331,10 +333,10 @@ while run:
     Platform.Platform_Rect(Trampoline, player1, 95, 1, 31, 31, CountF)
     Platform.SetPlatform(500, platformtime, conveyor_left, CountF)
     Platform.Platform_Rect(conveyor_left, player1, 95, 1, 31, 31, CountF)
-    Platform.SetPlatform(1000 , platformtime,Board2 , CountF)
+    Platform.SetPlatform(1200 , platformtime,Board2 , CountF)
     Platform.Platform_Rect(Board2, player1, 94, 1, 31, 31, CountF)
     
-    Platform.SetPlatform(2000 , platformtime,Board3 , CountF)
+    Platform.SetPlatform(2400 , platformtime,Board3 , CountF)
     Platform.Platform_Rect(Board3, player1, 94, 1, 31, 31, CountF)
 
     Platform.SetPlatform(1500 , platformtime,Nails1, CountF )
@@ -353,7 +355,7 @@ while run:
 
          Platform.SetPlatform(timerand + timebios[7] , platformtime,Nails4, CountF )
          Platform.Platform_Rect(Nails4, player1, 95, 1, 31, 31, CountF) 
-                   
+         
   
     pygame.display.flip()#環境更新
 

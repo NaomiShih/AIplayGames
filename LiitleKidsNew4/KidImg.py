62fe04b965a6a3dbc6c6eec95aa9c1168460256c
john0927626983 <84298954+john0927626983@users.noreply.github.com>
2021-06-04 11:29:01 +0800

    Add files via upload

--- LiitleKidsNew4/KidImg.py
+++ LiitleKidsNew4/KidImg.py
@@ -7,7 +7,17 @@ Created on Wed Jun  2 11:15:17 2021
 import pygame
 from pygame.locals import *
 
-life = pygame.image.load('./assets/LIFE123.PNG')
+life  = pygame.image.load('./assets/LIFE123.PNG')
+life1 = pygame.image.load('./assets/LIFE123-1.PNG')
+life2 = pygame.image.load('./assets/LIFE123-2.PNG')
+life3 = pygame.image.load('./assets/LIFE123-3.PNG')
+life4 = pygame.image.load('./assets/LIFE123-4.PNG')
+life5 = pygame.image.load('./assets/LIFE123-5.PNG')
+life6 = pygame.image.load('./assets/LIFE123-6.PNG')
+life7 = pygame.image.load('./assets/LIFE123-7.PNG')
+life8 = pygame.image.load('./assets/LIFE123-8.PNG')
+life9 = pygame.image.load('./assets/LIFE123-9.PNG')
+life10 = pygame.image.load('./assets/LIFE123-10.PNG')
 player = pygame.image.load('./assets/playerNomal.png')
 
 Normal = pygame.image.load('./assets/playerNomal.png')
@@ -22,3 +32,4 @@ TrampolineUP = pygame.image.load('./assets/trampolineUP.png')
 TrampolineDown = pygame.image.load('./assets/trampolineDown.png')
 conveyor_left = pygame.image.load('./assets/conveyor_left1.png')
 conveyor_right = pygame.image.load('./assets/conveyor_right1.png')
+
